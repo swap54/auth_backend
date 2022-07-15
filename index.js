2fd2352dadee01195526b7393ce5dfa045a68301
swap54 <krswapnil84@gmail.com>
2022-07-15 16:29:44 +0530

    second commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://auth-frontend-five.vercel.app/",
     credentials:true
 }));
 app.use(session({
