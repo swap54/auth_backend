93ebaebda63f09d1dcd1333a7712d5816ad1ed64
swap54 <krswapnil84@gmail.com>
2022-07-15 16:15:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const passport = require('passport');
+const passportLocal = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const cookieSession = require('cookie-parser');
+const bodyParser  =require('body-parser');
+const cookieParser = require('cookie-parser');
+const User = require('./user')
+
+mongoose.connect('mongodb+srv://new_user:QJ50mjZuRxCF5EMN@cluster0.7gatw.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true 
+}).then( () => {
+    console.log('Connected to the database ')
+})
+.catch( (err) => {
+    console.error(`Error connecting to the database. n${err}`);
+})
+
+//Middleware
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+}));
+app.use(session({
+    secret: "secretcode",
+    resave:true,
+    saveUninitialized:true
+}))
+app.use(cookieParser("secretcode"))
+app.use(passport.initialize());
+app.use(passport.session());
+require('./passportConfig')(passport);
+
+
+//Routes
+app.post('/register',(req,res)=>{
+    User.findOne({username:req.body.username}, async(err,doc)=>{
+        if(err) throw err;
+        if(doc) res.send({message:"User already exists"});
+        if(!doc){
+            const hashedPassword = await bcrypt.hash(req.body.password,10);
+            const newUser = new User({
+                username:req.body.username,
+                password:hashedPassword,
+            });
+            await newUser.save();
+            res.send("User created");
+        }
+    });
+    
+});
+app.post('/login',(req,res,next)=>{
+    passport.authenticate("local",(err,user, info)=>{
+        if(err) throw err;
+        if(!user) res.send({message:"No user exists"});
+        else{
+            req.logIn(user,err=>{
+                if(err) throw err;
+                res.send(req.user);
+                console.log(req.user);
+            })
+        }
+    })(req,res,next)
+})
+app.get('/user',(req,res)=>{
+    res.send(req.user);
+})
+app.listen(3001,()=>{
+    console.log("server running");
+})
---
+++ passportConfig.js
@@ -0,0 +1,33 @@
+const User = require('./user');
+const bcrypt = require('bcryptjs');
+const localStratergy = require('passport-local').Strategy;
+
+module.exports = function(passport){
+    passport.use(
+        new localStratergy((username, password, done)=>{
+            User.findOne({username:username},(err,user)=>{
+                if(err) throw err;
+                if(!user) return done(null,false);
+                bcrypt.compare(password,user.password,(err,result)=>{
+                    if(err) throw err;
+                    if(result === true){
+                        return done(null,user);
+                    } else{
+                        return done(null, false);
+                    }
+                });
+            });
+        })
+    );
+    passport.serializeUser((user,cb)=>{
+        cb(null,user.id);
+    });
+    passport.deserializeUser((id,cb)=>{
+        User.findOne({_id:id},(err,user)=>{
+            const userInfo ={
+                username:user.username,
+            };
+            cb(err,userInfo);
+        })
+    })
+}
